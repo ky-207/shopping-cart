d3b511ceead2137c35d7c8c551230fc323799b51
kristyyip <59596642+kristyyip@users.noreply.github.com>
2020-02-07 18:59:10 -0500

    Look up product given identifier

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# shopping-cart
+# shopping-cart project
+
+[Project Description]
+(https://github.com/prof-rossetti/intro-to-python/blob/master/projects/shopping-cart/README.md)
+
+## Installation
+
+Clone or download from [GitHub Sorce](https://github.com/YOUR_USERNAME/shopping-cart), then navigate into the project repository:
+
+```sh
+cd shopping-cart
+```
+
+## Usage
+
+Run the program:
+
+```py
+python shopping_cart.py
+```
--- shopping_cart.py
+++ shopping_cart.py
@@ -25,7 +25,15 @@ products = [
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
-print(products)
+# print(products)
 # pprint(products)
 
 # TODO: write some Python code here to produce the desired output
+
+# capturing user inputs
+product_id = input("Please input a product identifier: ")
+matching_products = [p for p in products if str(p["id"]) == str(product_id)] # need to convert both to string so that they can be compared
+matching_product = matching_products[0]
+print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+
+# info display / output
